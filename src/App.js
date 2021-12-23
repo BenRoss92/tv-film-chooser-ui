78f4c795d97c91365144d5cb709de452edad79aa
Ben Ross <benjamin.ross@youview.com>
2021-12-23 16:22:14 +0000

    Display TV/Film items from a locally stored array

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const contentItems = [
+    {
+      id: 1,
+      title: 'Terminator 1',
+      runtime: 60
+    },
+    {
+      id: 2,
+      title: 'Terminator 2',
+      runtime: 70
+    },
+    {
+      id: 3,
+      title: 'Terminator 3',
+      runtime: 80
+    },
+    {
+      id: 4,
+      title: 'Terminator 4',
+      runtime: 90
+    },
+    {
+      id: 5,
+      title: 'Terminator 5',
+      runtime: 100
+    },
+  ];
+
+  const contentRows = contentItems.map((contentItem) => 
+    <tr key={contentItem.id}>
+      <td>
+        {contentItem.title}
+      </td>
+      <td>
+        {contentItem.runtime}
+      </td>
+    </tr>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="content-table-container">
+      <table>
+        <thead>
+        <tr>
+          <th className="table-heading-title">
+            Title
+          </th>
+          <th>
+            Runtime (mins)
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+          {contentRows}
+        </tbody>
+      </table>
+      </div>
   );
 }
 
